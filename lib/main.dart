2489a070bcf5c6883f6e1395e53e9251eaabe7b9
BlazEffect <kamalinigor@gmail.com>
2023-09-08 17:01:15 +0300

    Changed the names and added routes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Планировщик',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -33,7 +33,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => const HomePage(),
+        '/search': (BuildContext context) => const SearchPage(),
+      },
     );
   }
 }
