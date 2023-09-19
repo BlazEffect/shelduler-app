803b3519bcd3d5afe8479ec367b6af86e17db97f
BlazEffect <kamalinigor@gmail.com>
2023-09-19 10:18:33 +0300

    Added a connection to the database and output a list of tasks

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mysql_client/mysql_client.dart';
 
 import 'package:scheduler/blocks/AppNavigationBar.dart';
 
@@ -11,12 +12,120 @@ List<String> titles = <String>[
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Future<MySQLConnection> getConnection() async {
+    final conn = await MySQLConnection.createConnection(
+      host: "mysql-8.0",
+      port: 3306,
+      userName: "root",
+      password: "1234",
+      databaseName: "shelduler-mobile", // optional
+    );
+
+    await conn.connect();
+
+    return conn;
+  }
+
+  Future<Iterable<ResultSetRow>> getTasks() async {
+    final conn = await getConnection();
+    final results = await conn.execute('SELECT * FROM tasks');
+    return results.rows;
+  }
+
+  List<Widget> createTaskItems(Iterable<ResultSetRow> rowTasks) {
+    List<Widget> tasks = <Widget>[];
+
+    for(var rowTask in rowTasks) {
+      String name = rowTask.assoc()['name'] ?? '';
+
+      tasks.add(
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Container(
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(
+              child: Text(name)
+            ),
+          )
+        )
+      );
+    }
+
+    return tasks;
+  }
+
   @override
   Widget build(BuildContext context) {
     const int tabsCount = 3;
 
-    return DefaultTabController(
-        initialIndex: 1,
+    return FutureBuilder(
+      future: getTasks(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        
+        final tasks = createTaskItems(snapshot.data);
+
+        return DefaultTabController(
+          initialIndex: 0,
+          length: tabsCount,
+          child: Scaffold(
+            appBar: AppBar(
+              notificationPredicate: (ScrollNotification notification) {
+                return notification.depth == 1;
+              },
+              scrolledUnderElevation: 4.0,
+              shadowColor: Theme.of(context).shadowColor,
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    icon: const Icon(Icons.cloud_outlined),
+                    text: titles[0],
+                  ),
+                  Tab(
+                    icon: const Icon(Icons.list),
+                    text: titles[1],
+                  ),
+                  Tab(
+                    icon: const Icon(Icons.edit),
+                    text: titles[2],
+                  ),
+                ],
+              ),
+            ),
+            body: ListView(
+                padding: const EdgeInsets.all(20),
+                children: tasks
+            ),
+            floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                tooltip: 'Create',
+                shape: const CircleBorder(),
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white,
+                child: const Icon(Icons.add)
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            bottomNavigationBar: const AppNavigationBar(
+              fabLocation: FloatingActionButtonLocation.centerDocked,
+              shape: CircularNotchedRectangle(),
+            ),
+          )
+        );
+      }
+    );
+
+    /*return DefaultTabController(
+        initialIndex: 0,
         length: tabsCount,
         child: Scaffold(
           appBar: AppBar(
@@ -42,6 +151,10 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
+          body: ListView(
+            padding: const EdgeInsets.all(20),
+            children: tasks
+          ),
           floatingActionButton: FloatingActionButton(
               onPressed: () {},
               tooltip: 'Create',
@@ -56,6 +169,6 @@ class HomePage extends StatelessWidget {
             shape: CircularNotchedRectangle(),
           ),
         )
-    );
+    );*/
   }
 }
