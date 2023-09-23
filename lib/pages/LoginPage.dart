e8c26c13a2b53a8a0846d405030f498f8afda23f
BlazEffect <kamalinigor@gmail.com>
2023-09-23 12:31:18 +0300

    Made the markup of the login page

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -7,8 +7,52 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: AppNavigationBar(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Авторизация'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const TextField(
+            decoration: InputDecoration(labelText: 'Email'),
+          ),
+          const TextField(
+            decoration: InputDecoration(labelText: 'Password'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+                onPressed: () {  },
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 16),
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                  elevation: 5.0,
+                  padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                ),
+                child: const Text('Войти')
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 16),
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                  elevation: 5.0,
+                  padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                ),
+                child: const Text('Зарегестрироваться')
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: const AppNavigationBar(
         fabLocation: FloatingActionButtonLocation.centerDocked,
         shape: CircularNotchedRectangle(),
       )
