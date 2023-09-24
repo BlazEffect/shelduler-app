e2331544bb1cabca91e2093b77cc36f216e4ad12
BlazEffect <kamalinigor@gmail.com>
2023-09-24 21:19:24 +0300

    Added methods for getting all values and by id to the task model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,8 +1,10 @@
+import 'package:scheduler/utils/base_model.dart';
+
 class Task {
-  int id;
+  String id;
   String name;
   String description;
-  int userId;
+  String userId;
 
   Task({
     required this.id,
@@ -30,5 +32,42 @@ class Task {
   }
 }
 
-class TaskModel {
+class TaskModel extends BaseModel {
+  @override
+  String table = "tasks";
+
+  @override
+  getById(String id) async {
+    var data = await super.getById(id);
+
+    if (data is Map) {
+      var t = Task(
+          id: data["id"],
+          name: data["name"],
+          description: data["description"],
+          userId: data["user_id"] ?? ""
+      );
+
+      return t;
+    }
+  }
+
+  @override
+  getAll() async {
+    var data = await super.getAll();
+    List<Task> tasks = [];
+
+    for (var task in data) {
+      tasks.add(
+        Task(
+          id: task.assoc()["id"],
+          name: task.assoc()["name"],
+          description: task.assoc()["description"],
+          userId: task.assoc()["userId"]
+        )
+      );
+    }
+
+    return tasks;
+  }
 }
