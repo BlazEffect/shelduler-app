2dafcfacac3f3e18ac0c2913b6bc0b8df2b4e284
BlazEffect <kamalinigor@gmail.com>
2023-09-24 21:18:15 +0300

    Created a base model

---
+++ lib/utils/base_model.dart
@@ -0,0 +1,23 @@
+import 'package:scheduler/database/db_provider.dart';
+
+class BaseModel {
+  late String table;
+
+  getById(String id) async {
+    final conn = await DBProvider.db.database;
+    final results = await conn?.execute("SELECT * FROM $table WHERE id=$id");
+
+    if (results!.numOfRows > 0) {
+      return results.rows.first.assoc();
+    }
+  }
+
+  getAll() async {
+    final conn = await DBProvider.db.database;
+    final results = await conn?.execute("SELECT * FROM $table");
+
+    if (results!.numOfRows > 0) {
+      return results.rows;
+    }
+  }
+}
