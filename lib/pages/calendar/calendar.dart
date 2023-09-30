be4904f420abdcde613a6cf9eb4ba4d6a7389ddc
BlazEffect <kamalinigor@gmail.com>
2023-09-30 18:47:34 +0300

    Added a calendar widget and created several labels

--- lib/pages/calendar/calendar.dart
+++ lib/pages/calendar/calendar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:scheduler/widgets/app_navigation_bar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({super.key});
@@ -12,49 +13,24 @@ class CalendarPage extends StatelessWidget {
         title: const Text('Календарь'),
         automaticallyImplyLeading: false,
       ),
-      body: CustomScrollView(
-        slivers: <Widget> [
-          SliverPadding(
-            padding: const EdgeInsets.all(20),
-            sliver: SliverGrid.count(
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              crossAxisCount: 3,
-              children: <Widget> [
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.green[100],
-                  child: const Text("He'd have you all unravel at the"),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.green[200],
-                  child: const Text('Heed not the rabble'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.green[300],
-                  child: const Text('Sound of screams but the'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.green[400],
-                  child: const Text('Who scream'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.green[500],
-                  child: const Text('Revolution is coming...'),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.green[600],
-                  child: const Text('Revolution, they...'),
-                ),
-              ],
-            ),
-          )
-        ]
+      body: SfCalendar(
+        view: CalendarView.month,
+        dataSource: MeetingDataSource(_getDataSource()),
+        /*timeSlotViewSettings: const TimeSlotViewSettings(
+          nonWorkingDays: <int>[DateTime.saturday, DateTime.sunday]
+        ),*/
+        monthViewSettings: const MonthViewSettings(showAgenda: true),
+        allowedViews: const <CalendarView>[
+          CalendarView.day,
+          CalendarView.week,
+          CalendarView.month,
+          CalendarView.timelineDay,
+          CalendarView.timelineWeek,
+          CalendarView.timelineMonth
+        ],
+        firstDayOfWeek: DateTime.monday,
+        showDatePickerButton: true,
+        showNavigationArrow: true,
       ),
       bottomNavigationBar: const AppNavigationBar(
         fabLocation: FloatingActionButtonLocation.centerDocked,
@@ -63,4 +39,62 @@ class CalendarPage extends StatelessWidget {
     );
   }
 
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime = DateTime(today.year, today.month, today.day, 9);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+    meetings.add(Meeting(
+        'Conference', startTime, endTime));
+    return meetings;
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
+  }
+}
+
+/// Custom business object class which contains properties to hold the detailed
+/// information about the event data which will be rendered in calendar.
+class Meeting {
+  /// Creates a meeting class with required details.
+  Meeting(this.eventName, this.from, this.to);
+
+  /// Event name which is equivalent to subject property of [Appointment].
+  String eventName;
+
+  /// From which is equivalent to start time property of [Appointment].
+  DateTime from;
+
+  /// To which is equivalent to end time property of [Appointment].
+  DateTime to;
 }
